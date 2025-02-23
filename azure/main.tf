685fde8b8a54f3ee17dc9f04605b65c1788a8586
alimajaf <50100268+alimajaf@users.noreply.github.com>
2025-02-23 13:53:07 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -33,7 +33,8 @@ resource "azurerm_network_interface" "nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.5"
   }
 }
 
